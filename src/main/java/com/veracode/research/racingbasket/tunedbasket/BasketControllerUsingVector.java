c23f8d3fa3ea6bf3d9ee8a20dbd0bfb8bbeaf21d
gtrovato <gtrovato@veracode.com>
2022-02-14 17:23:05 +0000

    Initial Vector controller

---
+++ src/main/java/com/veracode/research/racingbasket/tunedbasket/BasketControllerUsingVector.java
@@ -0,0 +1,8 @@
+package com.veracode.research.racingbasket.tunedbasket;
+
+/**
+ * Implement the example with the use of Vector class and one with ArrayList. Reason is that the Vector object is
+ * always syncronized whilst ArrayList is not;
+ */
+public class BasketControllerUsingVector {
+}
